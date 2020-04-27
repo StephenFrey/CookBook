a4183c363a7b1509a3515e196360076e95b1b808
StephenFrey <57848822+StephenFrey@users.noreply.github.com>
2020-04-26 19:36:42 -0600

    Update TickManager.py

--- Pythonista/GameDevelopment/TickManager.py
+++ Pythonista/GameDevelopment/TickManager.py
@@ -19,14 +19,14 @@
 	LabelNode from Pythonista's scene module to display Tick info on screen.
 	
 '''
-from scene import *
+import scene
 class Tick:
 	val=0.0 
 	last_tick=0.0
 	intervals=1.0
 	objects_to_update=[]
-	label=LabelNode(text='Tick: ', color=(0.0, 0.0, 0.0, 1.0), 
-					size=Size(50, 35), anchor_point=(0.0, 0.0))
+	label=scene.LabelNode(text='Tick: ', color=(0.0, 0.0, 0.0, 1.0), 
+					size=scene.Size(50, 35), anchor_point=(0.0, 0.0))
 	
 	@classmethod
 	def dt(cls): 
@@ -58,7 +58,7 @@ class Tick:
 			for child in cls.objects_to_update:
 				child.update()
 
-class GameLoop(Scene):
+class GameLoop(scene.Scene):
 	def setup(self):
 		pass
 	
